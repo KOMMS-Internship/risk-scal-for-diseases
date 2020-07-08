4ccb7289d03416df7b170b4238048775aee20278
Rasmus <rasmus.wannenmacher@web.de>
2020-07-08 11:29:02 +0200

    Habe mal noch Inkubationszeit dazugemacht

--- riskfactor.py
+++ riskfactor.py
@@ -2,6 +2,7 @@ while True:
     M = float(input('Mortalität als Dezimalzahl'))
     I = float(input('Infektionswahrscheinlichkeit beim x Minuten in einem x großen Raum mit einer infizierten Person aufhalten'))
     A = float(input('gefährdeteste Altersstufe'))
+    In = float(input('Inkubationszeit in Tagen(Durchschnitt)'))
     if A > 60:
         A1 = 1
     if A > 30 and A <= 60:
@@ -10,9 +11,10 @@ while True:
         A1 = 3
 
     alpha = 0.4
-    beta = 0.5
+    beta = 0.4
     delta = 0.1
+    gamma = 0.1
 
-    riskscale = M*alpha + I*beta + A1*delta
+    riskscale = M*alpha + I*beta + A1*delta + In*gamma
 
     print(riskscale)
