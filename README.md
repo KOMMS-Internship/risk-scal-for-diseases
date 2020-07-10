415177c3d78379bb896dbd1f34919d6dc955d1d2
Paul <p.k.m@outlook.de>
2020-07-10 12:29:12 +0200

    Added criteria and formula to evaluate the resilience of the local (health care) system.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Disease Danger
+
 This repository is used to create a formula which enables people to rank diseases according to their dangerousness.
--- riskfactor.py
+++ riskfactor.py
@@ -1,7 +1,7 @@
 while True:
     M = float(input('Mortalität als Dezimalzahl'))
     I = float(input('Infektionswahrscheinlichkeit beim x Minuten in einem x großen Raum mit einer infizierten Person aufhalten'))
-    A = float(input('gefährdeteste Altersstufe'))
+    A = float(input('gefährdetste Altersstufe'))
     In = float(input('Inkubationszeit in Tagen(Durchschnitt)'))
     IA = float(input('Anstecken in Inkubationszeit? nicht anstecken = 0.1 Ansteckend = 1'))
     if A > 60:
@@ -19,3 +19,34 @@ while True:
     riskscale = M*alpha + I*beta + A1*delta + In*gamma*IA
 
     print(riskscale)
+
+    """
+    Kriterien zur Bewertung des Gesundheitssystems basierend auf den Kriterien der 
+    Joint Commission on Accreditation of Healthcare Organizations (JCAHO):
+    
+    - Zugänglichkeit der Versorgung
+    - Angemessenheit der Versorgung
+    - Stetigkeit/Koordination der Versorgung
+    - Wirksamkeit unter Idealbedingungen (Efficacy of care)
+    - Wirksamkeit in der Versorgungspraxis (Effectiveness of care)
+    - Wirtschaftlichkeit der Versorgung (Efficiency of care)
+    - Patientenorientierung der Versorgung
+    - Sicherheit der Versorgungsumgebung
+    - Rechtzeitigkeit der Versorgung
+    
+    Meine Vorschläge für konkrete Zahlen: 
+    
+    - Ärzte pro Einwohner
+    - Krankenhausbetten pro Einwohner
+    - BIP
+    - Bevölkerungsdichte
+    """
+    addSystem = bool(input("Systemscore berechnen?"))
+    if addSystem:
+        doctors_per_person = float(input("Ärzte pro Einwohner:"))
+        beds_per_person = float(input("Krankenhausbetten pro Einwohner:"))
+        bip = float(input("BIP:"))
+        population_density = float(input("Bevölkerungsdichte:"))
+
+        systemscore = (doctors_per_person * beds_per_person * bip)/population_density
+        print(population_density)
